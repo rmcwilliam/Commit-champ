5d8fb4f7745eb039e6655545532e03f69025548d
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-10-16 19:49:03 -0400

    started

--- lib/commitchamp.rb
+++ lib/commitchamp.rb
@@ -2,18 +2,69 @@ require "httparty"
 require "pry"
 
 require "commitchamp/version"
-# Probably you also want to add a class for talking to github.
+require "commitchamp/github_api"
 
 module Commitchamp
   class App
     def initialize
+      @weeks 
     end
 
     def run
-      # Your code goes here...
+      #table = {:username => "", :additions => "", :deletions => "", :changes => "" }
+      table = Array.new
+      additions = Array.new
+      
+
+      puts "Please enter your authentication token:"
+      token = STDIN.gets.chomp
+      auth = Githubapi.new(token)
+
+      puts "Please enter the owner of the repo you would like to view:"
+      owner = STDIN.gets.chomp
+
+      puts "Please enter the repo you are interested in viewing:"
+      repo = STDIN.gets.chomp
+
+      results = auth.get_contributions(owner, repo)  
+
+      results.each do |result|
+        username = result["author"]["login"]
+        @weeks = result["weeks"]
+        table.push(username)
+        #binding.pry
+      end
+    
+      @weeks.each do |week|
+       add = week["a"]
+       delete = week["d"]
+       commit = week["c"]
+       #additions.push(add)
+        binding.pry
+      end
+        sum = additions.inject {|sum, n| sum + n}
+        #binding.pry
     end
   end
 end
 
 app = Commitchamp::App.new
 app.run
+
+## Contributions for 'owner/repo'
+
+# Username      Additions     Deletions     Changes
+# User 1            13534          2954        6249
+# User 2             6940           913        1603
+
+# Prompt the user for an auth token
+# Ask the user what org/repo to get data about from github
+# Print a table of contributions ranked in various ways
+# Ask the user if they'd like to fetch another or quit.
+
+# You don't have to track contributions by week, just sum them to get a total.
+# Once all the contributions have been collected for a repo, offer to sort them by:
+# 1) lines added 2) lines deleted 3) total lines changed 4) commits made
+
+
+
---
+++ lib/commitchamp/github_api.rb
@@ -0,0 +1,22 @@
+require "httparty"
+require "pry"
+
+
+module Commitchamp  
+  class Githubapi
+    include HTTParty
+    base_uri "https://api.github.com"
+  
+      def initialize(auth_token)
+        @auth = {
+        "Authorization" => "token #{auth_token}",
+        "User-Agent"    => "HTTParty"
+          }
+      end
+
+      #GET /repos/:owner/:repo/stats/contributors
+      def get_contributions(owner, repo)
+        Githubapi.get("/repos/#{owner}/#{repo}/stats/contributors", :header => @auth)
+      end
+  end
+end
