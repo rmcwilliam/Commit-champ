5f4279fe0d9d9425e01e13aa8b4c7e8c43635d67
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-10-18 17:11:39 -0400

    added table structure; fixed typos etc.

--- lib/commitchamp.rb
+++ lib/commitchamp.rb
@@ -18,37 +18,57 @@ module Commitchamp
         additions = result["weeks"].inject(0) {|sum, key| sum + key["a"]} # must pass 0 to inject method to work?
         deletions = result["weeks"].inject(0) {|sum, key| sum + key["d"]}
         commits = result["weeks"].inject(0) {|sum, key| sum + key["c"]}
-        total_commits = additions + deletions + commits 
-        @user_data.push ({user: username, a: additions, d: deletions, c: commits, t: total_commits}) # Just create another array of hashes...
-        #binding.pry
+        total_lines_changed = additions + deletions  
+        @user_data.push ({user: username, a: additions, d: deletions, c: commits, t: total_lines_changed}) # Just create another array of hashes...
       end
+      puts "\n\n"
       table(@user_data)
+      puts "\n\n"
       sort_data
+      options
     end
 
     def sort_data
-      puts "Would you like to sort the data by additions: a, deletions: d, or total commits: t?"
-      puts "Please select a, d, or t"
-      selection = STDIN.gets.chomp
+      puts "Would you like to sort the data by additions: (a), deletions: (d), or total lines changed: (t)?\n\n"
+      puts "Please select: ( a ), ( d ), or ( t ) if you would like to sort, otherwise selct ( o )"
+      selection = STDIN.gets.chomp.downcase
       if selection == "a"
         data = @user_data.sort_by {|value| value[:a]}
         table(data)                                     
-        #binding.pry
       end
       if selection == "d"
         data = @user_data.sort_by {|value| value[:d]}
         table(data)
       end
       if selection == "t"
-        t = @user_data.sort_by {|value| value[:t]}
+        data = @user_data.sort_by {|value| value[:t]}
         table(data)
       end
-      #binding.pry
+      if selection == "o"
+        options
+      end
+      puts "\n\n"
+     options
     end                                              
 
     def table(data)
+      printf("%-20s%10s%10s%10s%22s\n", "Username","Additions","Deletions","Commits","Total Lines Changed")
       data.each do |info|
-        print(info[:user], info[:a], info[:d], info[:c], info[:t]) # Find fix: display looks bad
+        printf("%-20s%08s%08s%11s%14s\n", info[:user], info[:a], info[:d], info[:c], info[:t]) 
+      end
+    end
+    
+    def options
+      puts "Would you like to sort the data differently: ( s ), select another repo: ( r ), or quit: ( q )"
+      option = STDIN.gets.chomp.downcase
+      if option == "s"
+        sort_data
+      end
+      if option == "r"
+        run
+      end
+      if option == "q"
+        exit
       end
     end
 
@@ -72,20 +92,4 @@ end
 app = Commitchamp::App.new
 app.run
 
-## Contributions for 'owner/repo'
-
-# Username      Additions     Deletions     Changes
-# User 1            13534          2954        6249
-# User 2             6940           913        1603
-
-# Prompt the user for an auth token
-# Ask the user what org/repo to get data about from github
-# Print a table of contributions ranked in various ways
-# Ask the user if they'd like to fetch another or quit.
-
-# You don't have to track contributions by week, just sum them to get a total.
-# Once all the contributions have been collected for a repo, offer to sort them by:
-# 1) lines added 2) lines deleted 3) total lines changed 4) commits made
-
-# Hash with usernames as keys for building up the table.
 
